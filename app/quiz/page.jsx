e5d5e02be52ef14d832c367a15dbddb05fb090b4
kophyogit <kophyo.xeno@gmail.com>
2023-11-03 14:53:43 +0630

    first commit

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,149 +1,136 @@
-'use client';
+"use client";
 import React, { useState, useEffect } from "react";
 import { quiz } from '../data.js';
-const page = () => {
+
+const getRandomQuestions = () => {
+  const shuffledQuestions = quiz.questions.sort(() => Math.random() - 0.5);
+  return shuffledQuestions.slice(0,10);
+};
+
+const Page = () => {
+  const [randomQuestions, setRandomQuestions] = useState(getRandomQuestions());
   const [activeQuestion, setActiveQuestion] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState('');
   const [selectedAnswers, setSelectedAnswers] = useState([]);
-  const [checked, setChecked] = useState(false);
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [showResult, setShowResult] = useState(false);
   const [result, setResult] = useState({
     score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
   });
-   //timer
-  const [minutes, setMinutes] = useState(0)
-  const [seconds, setSeconds] = useState(30)
-  useEffect(() => {
-    let interval;
+  const [timer, setTimer] = useState(10); 
 
-    if (minutes > 0 || seconds > 0) {
-      interval = setInterval(() => {
-        if (seconds === 0) {
-          if (minutes === 0) {
-            clearInterval(interval);
-          } else {
-            setMinutes((prev) => prev - 1);
-            setSeconds(59);
-          }
-        } else {
-          setSeconds((prev) => prev - 1);
+  useEffect(() => {
+    let timerInterval;
+
+    if (timer > 0 && !showResult) {
+      timerInterval = setInterval(() => {
+        setTimer(prev => prev - 1);
+        if (timer === 1) {
+          setShowResult(true); 
         }
       }, 1000);
-    } else {
-      setShowResult(true);
     }
 
-    return () => clearInterval(interval);
-}, [minutes, seconds]);
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [timer, showResult]);
+ 
+  const nextQuestion = () => {
+    setResult((prev) =>
+      selectedAnswers[activeQuestion]
+        ? {
+            ...prev,
+            score: prev.score + 1,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          } 
+    );
 
-  const { questions } = quiz;
-  const { question, answers, correctAnswer } = questions[activeQuestion];
+    if (activeQuestion !== randomQuestions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
+    } else {
+      setShowResult(true);
+      clearInterval(timer);
+    }
+  }
 
-  //restart
   const restartQuiz = () => {
-    setShowResult(false);
+    setActiveQuestion(0);
     setSelectedAnswers([]);
+    setShowResult(false);
     setResult({
       score: 0,
       correctAnswers: 0,
       wrongAnswers: 0,
     });
-    setMinutes(1);
-    setSeconds(0);
+    setRandomQuestions(getRandomQuestions());
+    setTimer(300); 
   };
-  //answers
+
   const onAnswerSelected = (answer, id) => {
-    setChecked(true);
-    setSelectedAnswerIndex(id);
-  
-    if (answer === correctAnswer) {
-      setSelectedAnswer(true);
-      console.log('true');
-    } else {
-      setSelectedAnswer(false);
-      console.log('false');
-    }
-  
     setSelectedAnswers(prevAnswers => {
       const updatedAnswers = [...prevAnswers];
       updatedAnswers[activeQuestion] = answer;
       return updatedAnswers;
     });
   };
-  //next
-  const nextQuestion = () => {
-    setSelectedAnswerIndex(null)
-    setResult((prev) =>
-    selectedAnswer
-    ? {
-        ...prev,
-        score: prev.score + 1,
-        correctAnswers: prev.correctAnswers + 1,
-    }
-    : {
-        ...prev,
-        wrongAnswers: prev.wrongAnswers + 1,
-    } 
-    );
-    if (activeQuestion !== questions.length - 1) {
-        setActiveQuestion((prev) => prev + 1);
-    } else {
-        setActiveQuestion(0);
-        setShowResult(true);
-    }
-    setChecked(false);
-  }
-    return (
-        <div className="container">
-            <h1>Quiz Page</h1>
-            <div>
-                <h2>
-                    Question: {activeQuestion + 1}
-                    <span>/{questions.length}</span>
-                </h2>
-            </div>
-            <div>
-                {!showResult ? (
-                    <div className='quiz-container'>
-                        <h3>{questions[activeQuestion].question}</h3>
-                        <div className="timer">
-                            <label>Timelimit</label><br></br>
-                            <span>{minutes}mins:{seconds}s</span>
-                        </div>
-                        {answers.map((answer, id) => (
-                            <li onClick={() => onAnswerSelected(answer, id)} key={id} className={selectedAnswerIndex === id ? 'li-selected' : 'li-hover'}>
-                                <span>{answer}</span>
-                            </li>
-                        ))}
-                        {checked ? (
-                            <button onClick={nextQuestion} className="btn">{activeQuestion === questions.length - 1 ? 'Submit' : 'Next'}</button>
-                        ) : (
-                            <button onClick={nextQuestion} disabled className="btn-disabled">
-                                {''}
-                                {activeQuestion === questions.length - 1 ? 'Submit' : 'Next'}
-                            </button>
-                        )}
-                    </div>
-                ) : (
-                    <div className='quiz-container'>
-                        <h3>Results</h3>
-                        <p>Score: <span>{result.score}/10</span></p>
-                        <h3>Your Answer</h3>
-                        {questions.map((q, index) => (
-                        <div key={index}>
-                          <span className="yourAnswer">Question {index + 1} - {selectedAnswers[index] ? selectedAnswers[index] : "Not answered"}</span>
-                          <span className="correctAnswer">Correct Answer: {q.correctAnswer}</span>
-            </div>
-))}
-                          <button onClick={restartQuiz} className="btn">Try Again</button>
-                    </div>
-)}
-            </div>
-        </div>
-    )
-}
 
-export default page
+  let questionComponent = null;
+  if (randomQuestions.length > 0) {
+    const { question, answers } = randomQuestions[activeQuestion];
+
+    questionComponent = (
+      <div className='quiz-container'>
+        <h3>{question}</h3>
+        <ul>
+          {answers.map((answer, id) => (
+            <li
+              onClick={() => onAnswerSelected(answer, id)}
+              key={id}
+              className={selectedAnswers[activeQuestion] === answers[id] ? 'li-selected' : 'li-hover'}
+            >
+              <span>{answer}</span>
+            </li>
+          ))}
+        </ul>
+        <button onClick={nextQuestion} className="btn">
+          {activeQuestion === randomQuestions.length - 1 ? 'Submit' : 'Next'}
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <h1>Quiz Page</h1>
+      <div>
+        <h2>Question: {activeQuestion + 1}/10</h2>
+        <h2>Time Remaining: {Math.floor(timer / 60)}:{(timer % 60).toString().padStart(2, '0')}</h2>
+      </div>
+      <div>
+        {!showResult ? (
+          questionComponent
+        ) : (
+          <div className='quiz-container'>
+            <h3>Results</h3>
+            <p>Score: <span>{result.score}/10</span></p>
+            <h3>Your Answers</h3>
+            {randomQuestions.map((q, index) => (
+              <div key={index}>
+                <span className="yourAnswer">Question {index + 1} - {selectedAnswers[index] ? selectedAnswers[index] : "Not answered"}</span>
+                <span className="correctAnswer">Correct Answer: {q.answers[q.correct_index]}</span>
+              </div>
+            ))}
+            <button onClick={restartQuiz} className="btn">Try Again</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
